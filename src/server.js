8da49b6d1f6a17751a0e51c4d360821636be60f8
Adventureandre <adventureandre@hotmail.com>
2023-12-03 17:05:07 -0300

    trabalhando com Stream

--- src/server.js
+++ src/server.js
@@ -2,31 +2,46 @@ import http from 'node:http'
 
 const users = []
 
-const server  = http.createServer((req,res)=>{
-const {method, url} = req
+const server = http.createServer(async (req, res) => {
+    const {method, url} = req
 
-    if(method === "GET" && url === '/users'){
-       return res
-       .setHeader('Content-type','application/json')
-       .end(JSON.stringify(users))
+//Buffer Stream
+    const buffers = [];
+
+    for await(const chunk of req) {
+        buffers.push(chunk)
     }
 
-    if(method === 'POST' && url === '/users'){
-       
-       users.push({
-        id:1,
-        name:'andre',
-        email:"adventureandre@hotmail.com"
-       })
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString());
 
-       return res.writeHead(201).end()
+    } catch (e) {
+        req.body = null
+    }
+
+
+    if (method === "GET" && url === '/users') {
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        const {name, email} = req.body;
+        users.push({
+            id: 1,
+            name: name,
+            email: email
+        })
+
+        return res.writeHead(201).end()
     }
 
 
     console.log(method, url)
 
 
-return res.writeHead(404).end("Rota nao existe")
+    return res.writeHead(404).end("Rota nao existe")
 
 
 })
